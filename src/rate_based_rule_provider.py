5bbfb808f41d3e048d3a66b83248fa7411e16300
kkessels <kevinkessels@binx.io>
2018-10-23 17:19:25 +0200

    Request formatting fix

--- src/rate_based_rule_provider.py
+++ src/rate_based_rule_provider.py
@@ -124,7 +124,9 @@ class RateBasedRuleProvider(ResourceProvider):
 
         updates = {'RuleId': self.physical_resource_id}
         updates.update({'RateLimit': self.properties['RateLimit']})
-        updates.update({'Updates': [deletes, inserts]})   # merge delete and insert set
+        merged_list = [deletes, inserts]
+        if merged_list:
+            updates.update({'Updates': [deletes, inserts]})   # merge delete and insert set
 
         try:
             updates.update({'ChangeToken': client.get_change_token()['ChangeToken']})
